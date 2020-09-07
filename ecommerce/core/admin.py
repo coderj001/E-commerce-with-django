f0047a4818ad5d7c8550490095aad71ad42d97ea
mr-zero <amirajubolchi001@gmail.com>
2020-09-07 12:49:23 +0530

    add and config allauth

--- ecommerce/core/admin.py
+++ ecommerce/core/admin.py
@@ -1,3 +1,17 @@
+from core.models import Item, Order, OrderItem
 from django.contrib import admin
 
-# Register your models here.
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(OrderItem)
+class ItemOrderItem(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Order)
+class ItemOrder(admin.ModelAdmin):
+    pass
--- ecommerce/core/models.py
+++ ecommerce/core/models.py
@@ -1,3 +1,27 @@
+import uuid
+
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    id=models.UUIDField(primary_key=True,editable=False,default=uuid.uuid4)
+    title=models.CharField(max_length=255,blank=False,verbose_name="Item title")
+    price=models.FloatField(verbose_name="Item price")
+
+    def __str__(self):
+        return self.title
+
+class OrderItem(models.Model):
+    id=models.AutoField(primary_key=True,editable=False)
+    item=models.ForeignKey(Item, on_delete=models.CASCADE)
+
+class Order(models.Model):
+    id=models.UUIDField(primary_key=True,editable=False,default=uuid.uuid5)
+    user=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    items=models.ManyToManyField(OrderItem)
+    ordered_date=models.DateTimeField(auto_now_add=True)
+    ordered=models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
